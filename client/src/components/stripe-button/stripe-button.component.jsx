25335069076d75d983c1e4513291762443a628a2
Arnab <arnab.bhattacharya28122000@gmail.com>
2021-02-04 02:05:45 +0530

    Added mobile view support

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -4,24 +4,26 @@ import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
 
   const onToken = token => {
     axios({
       url: 'payment',
-      mathos: 'post',
+      method: 'post',
       data: {
         amount: priceForStripe,
-        token
+        token: token
       }
-    }).then(response => {
-      alert('Payment Successful');
-    }).catch(error => {
-      console.log('Payment error: ', JSON.parse(error));
-      alert(
-        'There was an issue with your payment, please make sure you used the provided credit card'
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
         );
-    });
+      });
   };
 
   return (
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app= express();
 const port = process.env.PORT || 5000;
 
@@ -23,5 +25,21 @@ if(process.env.NODE_ENV==='production'){
 
 app.listen(port, error => {
     if (error) throw error;
-    console.log('Server running on port 5000');
+    console.log('Server running on port '+port);
+});
+
+app.post('/payment', (req, res) => {
+    const body ={
+        source: req.body.token.id,
+        amount: req.body.amount,
+        current: 'usd'
+    };
+
+    stripe.charges.create(body, (stripeErr, stripeRes) =>{
+        if(stripeErr){
+            res.status(500).send({ error: stripeErr});
+        }else{
+            res.status(200).send({ succes: stripeRes});
+        }
+    });
 });
